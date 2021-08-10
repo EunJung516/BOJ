74c3fb2cd42dc76d16128c1cb71247f69571d995
EunJung516 <0516dmswjd@gmail.com>
2021-08-11 01:11:05 +0900

    1005번 다시 풀이함

--- 00001~05000/1005_ACM Craft.cpp
+++ 00001~05000/1005_ACM Craft.cpp
@@ -1,41 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <cstring>
 using namespace std;
 
-int T, N, K, X, Y, W, D;
+int T, N, K, X, Y, W;
+int D[1010], vis[1010];
+vector<vector<int>> v;
 
-void solution(map<int,int>& buildings, map<int, bool>& check, vector<vector<int>>& subBuild, int W) {
-    if (subBuild[W - 1].size() == 0 || check[W]) return;
-
-    check[W] = true;    // 건설시간 계산된 빌딩임을 나타내도록 확인
-    for (int i = 0; i < subBuild[W - 1].size(); i++) {    // 건설하기 전에 건설해야 할 건물의 건설시간 계산
-        solution(buildings, check, subBuild, subBuild[W - 1][i]);
-    }
-
-    // 건설하기 전에 건설해야 할 건물들 중 최대 건설시간을 해당 건설시간에 추가
-    int max = buildings[subBuild[W - 1][0]];
-    for (int i = 0; i < subBuild[W - 1].size(); i++) {
-        if (max < buildings[subBuild[W - 1][i]]) max = buildings[subBuild[W - 1][i]];
-    }
-    buildings[W] += max;
+void dfs(int u) {
+	if (vis[u]) return;
+	int maxV = 0; vis[u] = 1;
+	for (int i : v[u]) {
+		dfs(i); 
+		if (maxV < D[i]) maxV = D[i];    // 이전에 건설해야 할 건물들에 대한 최대 시간 갱신
+	}
+	D[u] += maxV;    // 본래 시간에, 이전에 건설해야 할 건물들의 시간 추가
 }
 
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
-    cin >> T; while (T--) {
-        cin >> N >> K; 
-        map<int, int> buildings;    // 건설시간 저장
-        map<int, bool> check;    // 해당 건물의 총 건설시간을 계산했는지 확인
-        vector<vector<int>> subBuild(N);    // 건설하기 전에 건설해야 할 건물들을 저장
-        for (int i = 1; i <= N; i++) {
-            cin >> D; buildings[i] = D; check[i] = false;
-        }
-        while (K--) {
-            cin >> X >> Y; subBuild[Y-1].push_back(X);
-        }
-        cin >> W;
-        solution(buildings, check, subBuild, W);
-        cout << buildings[W] << endl;
-    }
+	ios::sync_with_stdio(0), cin.tie(0);
+	cin >> T; while (T--) {
+		cin >> N >> K; 
+		v.clear(); v.resize(N + 1); memset(vis, 0, sizeof vis);    // 초기화
+		for (int i = 1; i <= N; ++i) cin >> D[i];
+		while (K--) {
+			cin >> X >> Y; v[Y].push_back(X);
+		}
+		cin >> W; dfs(W);
+		cout << D[W] << '\n';    // 건설완료 하는데 드는 최소 시간 출력
+	}
 }
